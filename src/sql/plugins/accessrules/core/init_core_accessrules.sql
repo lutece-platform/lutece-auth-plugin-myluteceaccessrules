0e0cdb3c758d3e6bc2e771de303a92d469db2d10
FrancoisEricMerlin <FrancoisEricMerlin>
2022-09-22 17:07:54 +0200

    PMAR-1:Init Project: ADd properties Group

--- src/sql/plugins/accessrules/core/init_core_accessrules.sql
+++ src/sql/plugins/accessrules/core/init_core_accessrules.sql
@@ -13,3 +13,4 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 DELETE FROM core_user_right WHERE id_right = 'ACCESSRULES_MANAGEMENT';
 INSERT INTO core_user_right (id_right,id_user) VALUES ('ACCESSRULES_MANAGEMENT',1);
 
+INSERT INTO core_datastore VALUES ('myluteceaccessrules.site_property.application_code', "");
