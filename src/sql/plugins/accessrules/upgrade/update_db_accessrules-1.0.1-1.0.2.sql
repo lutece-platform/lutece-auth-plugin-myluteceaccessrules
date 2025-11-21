021a43b5b846f1eefc44c1ebf5d34dd9da65f70b
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-11-21 09:04:20 +0100

    LUT-31247 : Add the possibility to manage sql updates with liquibase plugin

--- src/sql/plugins/accessrules/upgrade/update_db_accessrules-1.0.1-1.0.2.sql
+++ src/sql/plugins/accessrules/upgrade/update_db_accessrules-1.0.1-1.0.2.sql
@@ -1,1 +1,4 @@
+-- liquibase formatted sql
+-- changeset myluteceaccessrules:update_db_accessrules-1.0.1-1.0.2.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE mylutece_accessrules_rule add COLUMN encodebackurl SMALLINT;
